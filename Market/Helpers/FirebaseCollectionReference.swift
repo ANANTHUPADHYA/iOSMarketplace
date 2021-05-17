227d55390b25a575ab54f6643797fbd58bcb11b9
ANANTHUPADHYA <ananthupadhya10@gmail.com>
2021-05-16 20:30:13 -0700

    Adding helper functions

---
+++ Market/Helpers/FirebaseCollectionReference.swift
@@ -0,0 +1,15 @@
+import Foundation
+import FirebaseFirestore
+
+enum FCollectionReference: String {
+    case User
+    case Category
+    case Items
+    case Basket
+}
+
+func FirebaseReference(_ collectionReference: FCollectionReference) -> CollectionReference {
+    
+    return Firestore.firestore().collection(collectionReference.rawValue)
+}
+
