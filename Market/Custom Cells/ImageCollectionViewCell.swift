98c1c30d0a7bae87ca953ac3cbc7a8c1cce4a6e5
ANANTHUPADHYA <ananthupadhya10@gmail.com>
2021-05-16 20:21:48 -0700

    Adding items, item view, item controller

---
+++ Market/Custom Cells/ImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func setupImageWith(itemImage: UIImage) {
+        
+        imageView.image = itemImage
+    }
+    
+}
